51a6a8ab6492ee48cefb40ff14207974868d2aa1
Carlos Romellon <crromellon@outlook.com>
2024-10-19 20:55:49 -0600

    Comprobaciones para los procedures que muestran  el historial y los prod
    Se agregaron comprobaciones para 1 no mostrar los productos que no tengan stock, y otra comprobación para que el historial solo se muestre si el usuario ha comprado algo, utilizando la tabla ventas como referencia

--- src/DataBase/Archivos_sql/StoredProcedures/ObtainHist_call_historial.sql
+++ src/DataBase/Archivos_sql/StoredProcedures/ObtainHist_call_historial.sql
@@ -2,32 +2,50 @@ DELIMITER //
 
 CREATE PROCEDURE ObtenerHistorialVentas (
     IN p_id_usuario INT,
-    IN p_pagina INT
+    IN p_pagina INT,
+    OUT mensaje VARCHAR(255)
 )
 BEGIN
     DECLARE v_limite INT DEFAULT 5;
     DECLARE v_offset INT DEFAULT (p_pagina - 1) * v_limite;
+    DECLARE v_total_ventas INT;
 
-    SELECT 
-        v.fecha_compra,
-        p.nombre AS nombre_producto,
-        p.descripcion AS descripcion_producto,
-        p.marca,
-        (SELECT ip.prod_img 
-         FROM Imagenes_productos ip 
-         WHERE ip.id_producto = p.id_producto 
-         LIMIT 1) AS imagen_producto, -- todo esto es para solo seleccionar 1 imagen
-        i.precio_producto,
-        i.precio_envio
-    FROM Ventas v
-    JOIN Productos p ON v.id_producto = p.id_producto
-    JOIN Inventario i ON p.id_producto = i.id_producto
-    WHERE v.id_usuario = p_id_usuario -- aqui se filtra por el id del usuario
-    ORDER BY v.fecha_compra DESC
-    LIMIT v_limite OFFSET v_offset;
+    SELECT COUNT(*) INTO v_total_ventas
+    FROM Ventas
+    WHERE id_usuario = p_id_usuario; -- Couenta las veces que aparece el id del usuario dentro de la tabla ventas
+
+    IF v_total_ventas = 0 THEN -- y si no a parece el id del usuario en la tabla ventas, se manda el siguiente mensaje
+        SET mensaje = 'El usuario no ha comprado nada';
+    ELSE -- si si aparece se hacen join para mostrar su historial de compra
+		SELECT 
+			v.fecha_compra,
+			p.nombre AS nombre_producto,
+			p.descripcion AS descripcion_producto,
+			p.marca,
+			(SELECT ip.prod_img -- 
+			FROM Imagenes_productos ip 
+			WHERE ip.id_producto = p.id_producto 
+			LIMIT 1) AS imagen_producto, -- todo esto es para solo seleccionar 1 imagen
+			i.precio_producto,
+			i.precio_envio
+		FROM Ventas v
+		JOIN Productos p ON v.id_producto = p.id_producto
+		JOIN Inventario i ON p.id_producto = i.id_producto
+		WHERE v.id_usuario = p_id_usuario -- aqui se filtra por el id del usuario
+		ORDER BY v.fecha_compra DESC
+		LIMIT v_limite OFFSET v_offset;
+		SET mensaje = 'Ventas encontradas';
+    END IF;
     
 END //
 
 DELIMITER ;
 
-CALL ObtenerHistorialVentas(1, 1); -- llama a la pagina 1 del historial del usuario con el id 1
+/*
+CALL ObtenerHistorialVentas(
+    1, -- llama al usuario con el id 1
+    1, -- y la pagina 1 del historial de este mismo
+    @mensaje -- y el mensaje para que muestre si encuentra o no al usuario en la tabla ventas
+);  
+SELECT @mensaje; -- y con el mensaje se avisa si hay o no el usuario
+*/
--- src/DataBase/Archivos_sql/StoredProcedures/ObtainProd_call_product.sql
+++ src/DataBase/Archivos_sql/StoredProcedures/ObtainProd_call_product.sql
@@ -21,6 +21,8 @@ BEGIN
         Imagenes_productos ip ON p.id_producto = ip.id_producto
     JOIN 
         Inventario i ON p.id_producto = i.id_producto
+    WHERE 
+        p.is_product_paused = FALSE -- se filtra los productos pausados (sin stock)
     GROUP BY 
         p.id_producto, p.nombre, p.descripcion, p.marca, i.precio_producto, i.precio_envio, i.existencia
     LIMIT 15 OFFSET pagina;
@@ -29,4 +31,6 @@ END //
 
 DELIMITER ;
 
-CALL ObtenerProductos(1); -- llamada al procedure
+/*
+CALL ObtenerProductos(1); -- llamada al procedure, a la pagina 1 (los primero 15 productos)
+*/
