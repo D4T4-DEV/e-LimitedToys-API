3d690d1d415dfdc3949315cc132e49c7b0a92754
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-20 11:48:03 -0600

    Create Pwd_process.ts
    Se crean los procesos para hashear la contraseña, y comparar la contraseña

---
+++ src/Security/Pwd_process.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+import 'dotenv/config'; // configuración de dotenv
+const { PASSWORD_SALT_ROUNDS } = process.env;
+
+
+export const CompararContrasenias = async (pwd_plana: string, psw_hash: string) => {
+    const isMatch = await bcrypt.compare(pwd_plana, psw_hash);
+    return isMatch;
+}
+
+export const CifrarContrasenia = async (pwd_plana: string) => {
+    const psw_hash = await bcrypt.hash(pwd_plana, Number(PASSWORD_SALT_ROUNDS));
+    return psw_hash;
+}
