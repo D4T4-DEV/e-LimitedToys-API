56ab76f14c4e00f98a852ce9febc422fc605ee41
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-20 22:11:58 -0600

    Create Tokens.ts
    Se genera el archivo encargado de generar tokens, para pasarlos a la aplicacion

---
+++ src/Security/Tokens.ts
@@ -0,0 +1,24 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+type JsonData = { [key: string]: any };
+
+const { RSA_PRIVATE_KEY } = process.env;
+
+const options: SignOptions = {
+    algorithm: 'RS256',
+    expiresIn: '1h', // Tiempo de expiracion
+}
+
+export const GenerarToken = (payload: JsonData): string => {
+
+    if (!RSA_PRIVATE_KEY) {
+        throw new Error('La clave RSA no esta definida');
+    }
+    
+    try {
+        const token = jwt.sign(payload, RSA_PRIVATE_KEY as string, options);
+        return token;
+    } catch (err) {
+        throw err;
+    }
+};
