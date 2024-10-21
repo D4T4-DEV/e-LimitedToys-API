3761f89cd6c8a44bd0dc236451d00e3542faf391
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-20 22:12:06 -0600

    Create UsuarioInterface.ts

---
+++ src/Interfaces/UsuarioInterface.ts
@@ -0,0 +1,15 @@
+export interface UserData {
+    user_ID?: string;
+    nombres?: string;
+    apellidos?: string;
+    email?: string;
+    password?: string;
+    nickname?: string;
+    imagenPerfil?: string;
+    calle?: string;
+    referencia?: string;
+    pais?: string;
+    ciudad?: string;
+    colonia?: string;
+    codigoPostal?: number;
+}
