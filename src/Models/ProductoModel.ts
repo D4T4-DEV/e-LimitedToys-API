a0e91634b75b7b449c1474bbe89d447b5e2f3c58
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-22 00:02:42 -0600

    Update ProductoModel.ts
    Checamos previamente que la consulta tenga datos, ya que esta puede devolverse sin nada

--- src/Models/ProductoModel.ts
+++ src/Models/ProductoModel.ts
@@ -88,14 +88,21 @@ export const ObtenerProductos = async (data: string): Promise<Respuesta> => {
     const conn_MYSQL = await getConnectionMySQL();
 
     try {
-        const [result]: any[] = await conn_MYSQL.query(`CALL ObtenerProductos( ? )`, [data]);
-        const queryResult = result[0];
-        return { status: 200, message: `Se ha devuelto los 15 productos del indice ${data}`, data: {queryResult}};
+        const [result]: any = await conn_MYSQL.query(`CALL ObtenerProductos( ? )`, [data]);
+
+        // Tomamos lo que viene de la consulta, o bien asignamos un arreglo vacio
+        const productosData = result[0] || [];
+
+        if (productosData.length > 0) {
+            return { status: 200, message: `Se ha devuelto los 15 productos del indice ${data}`, data: { productosData } };
+        }
+
+        return { status: 200, message: `No hay productos` };
     } catch (error) {
         const customError = new Error(`ObtenerProductos() modelo ${error}`);
         (customError as any).statusCode = 500;
         throw customError;
     } finally {
-        (await conn_MYSQL).release();
+        conn_MYSQL.release();
     }
 }
