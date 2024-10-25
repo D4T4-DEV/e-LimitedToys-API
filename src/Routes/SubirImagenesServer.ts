896f918417d8cbf87b45ac0b5ea158fee8b2a1f2
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2024-10-24 19:51:32 -0600

    Aspectos para subir una imagen de manera general

---
+++ src/Routes/SubirImagenesServer.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response  } from 'express';
+import path from 'path';
+import upload from '../Multer/Configuracion_multer';
+import { proccessDecryptDataMiddleware } from '../Middlewares/Decripty_recive_data';
+const Router = express.Router();
+// Toma de las variables del archivo env (desestructuracion)
+const { DIR_UPLOAD } = process.env;
+
+
+// Respuesta que dara para saber el estado del servidor
+Router.post('/new',
+    upload.single('imagen'),
+(req: Request, res: Response, next: NextFunction) => {
+
+    // Si no tiene un archivo 
+    if (!req.file) {
+        res.status(400).json({ status: 400, message: 'No se ha cargado ningun archivo' });
+        return;
+    }
+
+    const pathData = req.body.path;
+    // Nombre de la subcarpeta dentro de la carpeta seleccionada para subir archivos
+    const subcarpeta = pathData || 'default'; // Si tiene alguna definida la usa, si no usa default
+    try {
+        const nombreArchivo = req.file.filename; // Nombre del archivo (opcional)
+        const ruta = path.join(DIR_UPLOAD!, subcarpeta, nombreArchivo); // Ruta completa donde se guardara el archivo siendo: uploads/${subcarpeta}/${nombre_archivo.algo}
+        res.status(200).json({path_relative: ruta});
+    } catch (error) {
+        // Pasamos el error al middleware de errores
+        next(error);
+    }
+})
+
+export default Router;
