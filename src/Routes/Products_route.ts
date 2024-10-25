3bd66731be22ed3de0e64e920a2167d994733558
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-10-24 23:40:17 -0600

    Update Products_route.ts

--- src/Routes/Products_route.ts
+++ src/Routes/Products_route.ts
@@ -4,11 +4,21 @@ import * as Controllers from '../Controllers/productosController';
 
 const router = express.Router();
 
-router.post('/new', proccessDecryptDataMiddleware, Controllers.AniadirProducto);
-router.put('/edit', proccessDecryptDataMiddleware, Controllers.EditarProducto);
-router.delete('/delete', proccessDecryptDataMiddleware, Controllers.EliminarProducto);
+
+/*
+    Posibles implementaciones
+*/
+
+// router.post('/new', proccessDecryptDataMiddleware, Controllers.AniadirProducto);
+// router.put('/edit', proccessDecryptDataMiddleware, Controllers.EditarProducto);
+// router.delete('/delete', proccessDecryptDataMiddleware, Controllers.EliminarProducto);
+
+/*
+    👆 Hasta aqui llegan las posibles implementaciones (aplica para sus servicios y controladores)
+*/
 
 router.get('/get/:indice_producto',  Controllers.ObtenerProductos);
+router.get('/get-for-id/:idProducto',  Controllers.ObtenerProductoID);
 router.get('/get/search/:indice/:filter',  Controllers.ObtenerProductosBuscador);
 
 export default router;
