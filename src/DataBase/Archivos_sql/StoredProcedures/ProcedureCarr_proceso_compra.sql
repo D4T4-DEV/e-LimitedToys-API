77142e4327eb70c560c6b28d2881f1a578e5a83e
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-11-09 11:57:28 -0600

    Update ProcedureCarr_proceso_compra.sql
    Mejoramos para poder checar antes si tiene productos en el carrito como medida de prevención

--- src/DataBase/Archivos_sql/StoredProcedures/ProcedureCarr_proceso_compra.sql
+++ src/DataBase/Archivos_sql/StoredProcedures/ProcedureCarr_proceso_compra.sql
@@ -3,31 +3,39 @@ DELIMITER //
 CREATE PROCEDURE ProcesarCompra(IN in_id_usuario INT, OUT mensaje VARCHAR(255))
 BEGIN
 
-CREATE TEMPORARY TABLE ProductosFaltantes AS 
-SELECT 
-    Carrito.id_carrito,
-    Carrito.id_producto,
-    Carrito.cantidad_seleccionada,
-    Inventario.existencia
-FROM 
-    Carrito
-JOIN 
-    Inventario ON Carrito.id_producto = Inventario.id_producto
-WHERE 
-    Carrito.id_usuario = in_id_usuario
-    AND Carrito.cantidad_seleccionada > Inventario.existencia;
+    -- Verifixamos antes que exista un carrito con ese usuario (esto para no ejecutar codigo innecesario)
+    IF EXISTS (
+        SELECT 1
+        FROM Carrito
+        WHERE id_usuario = in_id_usuario
+    ) THEN
+            CREATE TEMPORARY TABLE ProductosFaltantes AS 
+            SELECT 
+                Carrito.id_carrito,
+                Carrito.id_producto,
+                Carrito.cantidad_seleccionada,
+                Inventario.existencia
+            FROM 
+                Carrito
+            JOIN 
+                Inventario ON Carrito.id_producto = Inventario.id_producto
+            WHERE 
+                Carrito.id_usuario = in_id_usuario
+                AND Carrito.cantidad_seleccionada > Inventario.existencia;
 
-IF EXISTS (SELECT 1 FROM ProductosFaltantes) THEN
-		-- Si hay registros, devolver los datos
-        SELECT * FROM ProductosFaltantes;
+            IF EXISTS (SELECT 1 FROM ProductosFaltantes) THEN
+                    -- Si hay registros, devolver los datos
+                    SELECT * FROM ProductosFaltantes;
+                ELSE
+                        -- Si no hay registros, devolver un mensaje
+                        CALL FinalizarCompra(in_id_usuario);
+                        SET mensaje = 'Compra finalizada correctamente';
+                END IF;
+            -- Eliminar la tabla temporal después de su uso
+            DROP TEMPORARY TABLE ProductosFaltantes;
     ELSE
-            -- Si no hay registros, devolver un mensaje
-            CALL FinalizarCompra(in_id_usuario);
-			SET mensaje = 'Compra finalizada correctamente';
+        SET mensaje = 'Este usuario no cuenta con productos en el carrito';
     END IF;
-
-    -- Eliminar la tabla temporal después de su uso
-    DROP TEMPORARY TABLE ProductosFaltantes;
 END //
 
 DELIMITER ;
